8ed039de89c8d404d0ef4681f6c3bf8ce885dc36
Johans Ballestar <Johans_Ballestar@Dell.com>
2021-12-15 13:40:50 -0600

    dynamic nft

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/oUX79xE-Dn1hYo9g_ieQC5GUzYffdb1R',
+      accounts: ['0425ead7a45d3cac0539c3c678246b64acc176666d013272edda6a5c253c990e'],
+    },
+  },
+};
